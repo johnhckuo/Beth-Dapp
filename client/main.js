bec6a473d569266cc1bcf1dd2d3123c8bdb4f6d9
HTCTAIPEI\John_Kuo <john_kuo@htc.com>
2017-05-25 13:05:40 +0800

    init upload

--- client/main.js
+++ client/main.js
@@ -3,25 +3,27 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-var callJson = {
-    "private": "51ca1b67efb999415260ef43194ff90ffd72887c607edde8dfd433c58fc08b8e",
-    "gas_limit": 2000000
-};
-
-var faucet = {
-    "address": "d2e4ace3e8ab6debf8360321caeba2c3a15b7d63",
-    "amount": 1000000000000000000
-};
 
 
 
 Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
 
 });
 
 
 if (Meteor.isClient){
+
+  Template.hello.helpers({
+	'result':function(){
+
+		return Session.get('data');
+	}	
+	  
+	  
+	  
+  });
+
+
 
   Template.hello.events({
     'click .init':function(event, instance) {
@@ -41,16 +43,29 @@ if (Meteor.isClient){
       // });
     },
     'click .length':function(event, instance) {
-      // increment the counter when button is clicked
-      Meteor.http.call("POST","https://api.blockcypher.com/v1/beth/test/contracts/"+CongressAddr+"/getStakeholdersLength?token="+token, JSON.stringify(callJson) , function(error,result){
-           console.log(result);
-      });
+		
+		
+		Meteor.call('getStakeholderLength', 'init', function(err, res){
+			if(err){
+				alert('Error');
+			}else{
+				Session.set("data", res);
+				console.log(res);
+			}
+		});
+
     },
     'click .addMember':function(event, instance) {
-      // increment the counter when button is clicked
-      Meteor.http.call("POST","https://api.blockcypher.com/v1/beth/test/contracts/"+CongressAddr+"?token="+token,function(error,result){
-           console.log(result);
-      });
+		
+		Meteor.call('addMember', 'init', function(err, res){
+			if(err){
+				alert('Error');
+			}else{
+				Session.set("data", res);
+				console.log(res);
+			}
+		});
+
     },
   });
 
--- server/main.js
+++ server/main.js
@@ -1,10 +1,36 @@
 import { Meteor } from 'meteor/meteor';
 
+var callJson = {
+    "private": "51ca1b67efb999415260ef43194ff90ffd72887c607edde8dfd433c58fc08b8e",
+    "gas_limit": 2000000
+};
+
+var faucet = {
+    "address": "d2e4ace3e8ab6debf8360321caeba2c3a15b7d63",
+    "amount": 1000000000000000000
+};
+
+
 if (Meteor.isServer){
   Meteor.methods({
       'getContract': function(operation){
           console.log("Hello world"+operation);
           return "received";
-      }
+      },
+	  'getStakeholderLength':function(){
+		  Meteor.http.call("POST","https://api.blockcypher.com/v1/beth/test/contracts/"+CongressAddr+"/getStakeholdersLength?token="+token, JSON.stringify(callJson) , function(error,result){
+			   console.log(result);
+			   return "stakeholder length :"+result;
+		  });
+	  },
+	  'addMember':function(){
+		  Meteor.http.call("POST","https://api.blockcypher.com/v1/beth/test/contracts/"+CongressAddr+"?token="+token,function(error,result){
+			if (error)}{
+				return "error";
+			}
+			   console.log(result);
+			   return "success";
+		  }); 
+	  }
   });
 }
